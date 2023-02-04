cc5a82c25d9614866ae64b687ee2c05265996574
Sergey Tselovalnikov <sergey@serce.me>
2023-02-04 19:19:14 +1100

    optimise parser and fix prioity order

--- src/test/kotlin/me/serce/cfrules/lang/parsingTest.kt
+++ src/test/kotlin/me/serce/cfrules/lang/parsingTest.kt
@@ -5,22 +5,24 @@ import com.intellij.lang.LanguageExtensionPoint
 import com.intellij.testFramework.ParsingTestCase
 
 abstract class CfRulesParsingTestBase(baseDir: String) :
-    ParsingTestCase(baseDir, "cfrule", true, CfRulesParserDefinition()) {
+  ParsingTestCase(baseDir, "cfrule", true, CfRulesParserDefinition()) {
 
-    override fun setUp() {
-        super.setUp()
-        CoreApplicationEnvironment.registerExtensionPoint(
-            application.extensionArea,
-            "com.intellij.lang.braceMatcher",
-            LanguageExtensionPoint::class.java
-        )
-    }
+  override fun setUp() {
+    super.setUp()
+    CoreApplicationEnvironment.registerExtensionPoint(
+      application.extensionArea,
+      "com.intellij.lang.braceMatcher",
+      LanguageExtensionPoint::class.java
+    )
+  }
 
-    override fun getTestDataPath() = "src/test/resources"
+  override fun getTestDataPath() = "src/test/resources"
 }
 
 class CfRulesParsingTest : CfRulesParsingTestBase("fixtures/parser") {
-    fun testComments() = doTest(true)
-    fun testSample() = doTest(true)
-    fun testIPv6() = doTest(true)
+  fun testComments() = doTest(true)
+  fun testFunctionCall() = doTest(true)
+  fun testIPv6() = doTest(true)
+  fun testPriority() = doTest(true)
+  fun testSample() = doTest(true)
 }
